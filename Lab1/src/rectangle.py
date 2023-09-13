88011c02f74c00106cb69d172e0db882bfd57215
白思雨 <baisiyu@baisiyudeMacBook-Pro.local>
2023-09-13 12:44:42 -0700

    refactoring the given code

--- Lab1/src/rectangle.py
+++ Lab1/src/rectangle.py
@@ -5,21 +5,35 @@
  Note that there is no constructor or destructor,
  so a default one will be created for us.
 """
-class Rectangle:
-    def set_values(self, x, y):
-        self.width = x
-        self.height = y
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+
+    @abstractmethod
+    def set_values(self, width, height):
+        pass
+
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Rectangle(Shape):
+    def set_values(self, width, height):
+        self._width = width
+        self._height = height
 
     def area(self):
-        return self.width * self.height
+        return self._width * self._height
 
 
 if __name__ == "__main__":
     # Create a rectangle object
-    rect = Rectangle()
-
-    # Call a member function
-    rect.set_values(3, 4)
+    rect = Rectangle(3, 4)
 
     # Print out the area function
     print("area:", rect.area())
