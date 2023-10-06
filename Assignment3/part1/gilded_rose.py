d812a695e19482e81f336fe01f02efb18f78cf9e
白思雨 <baisiyu@baisiyudeMacBook-Pro.local>
2023-10-05 20:57:15 -0700

    update A3

--- Assignment3/part1/gilded_rose.py
+++ Assignment3/part1/gilded_rose.py
@@ -20,30 +20,36 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
+            # Deal with Sulfuras
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue
+            # Deal with Aged Brie
+            elif item.name == "Aged Brie":
+                item.quality = min(50, item.quality + 1)
+            # Deal with Backstage passes
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.sell_in > 10:
+                    item.quality += 1
+                elif 6 <= item.sell_in <= 10:
+                    item.quality += 2
+                elif 1 <= item.sell_in <= 5:
+                    item.quality += 3
+                # The item has expired
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    item.quality = 0
+                # Set the maximum quality to 50
+                if item.quality > 50:
+                    item.quality = 50
+            # Deal with normal items with the exception of Conjured items
+            else:
+                degrade = 1
+                # Conjured items degrade twice as fast as normal items
+                if "Conjured" in item.name:
+                    degrade *= 2
+                # The item has expired
+                if item.sell_in <= 0:
+
+                    degrade *= 2
+                # The minimum quality is 0
+                item.quality = max(0, item.quality - degrade)
+            item.sell_in -= 1
--- Assignment3/part1/test_gilded_rose.py
+++ Assignment3/part1/test_gilded_rose.py
@@ -9,8 +9,58 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals("foo", items[0].name)
+        self.assertEqual(0, items[0].quality)
 
+    def test_normal_item(self):
+        items = [Item("foo", 0, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(8, items[0].quality)
+
+    def test_Sulfuras(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 4, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(10, items[0].quality)
+
+    def test_Conjured(self):
+        items = [Item("Conjured", 4, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(8, items[0].quality)
+
+    def test_Aged_Brie_before_sell_date(self):
+        items = [Item("Aged Brie", 2, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(11, items[0].quality)
+
+    def test_Backstage_passes_more_than_10_days(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 15, 5)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(6, items[0].quality)
+
+    def test_Backstage_passes_less_than_10_days(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 9, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(12, items[0].quality)
+
+    def test_Backstage_passes_less_than_5_days(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 4, 12)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(3, items[0].sell_in)
+        self.assertEqual(15, items[0].quality)
+
+    def test_Backstage_passes_expired(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(-1, items[0].sell_in)
+        self.assertEqual(0, items[0].quality)
 
 if __name__ == '__main__':
     unittest.main()
